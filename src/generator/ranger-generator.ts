a239d6ff378f5be4bac2723ef6bad8340083c740
Benjamin Heimann <benjamin.heimann@outlook.de>
2023-04-05 22:45:13 +0200

    Refactoring

--- src/generator/ranger-generator.ts
+++ src/generator/ranger-generator.ts
@@ -1,22 +1,12 @@
-import {
-    isList,
-    isLiteral,
-    isNull,
-    isProperty,
-    isPropertyReference,
-    Property,
-    PropertyReference,
-    Value,
-} from '../language-server/generated/ast';
+import { isList, isLiteral, isNull, Property, PropertyReference, Value } from '../language-server/generated/ast';
+import { resolveValue } from '../language-server/ranger-scope';
 import { DynamicObject } from '../utils/types';
 
 /**
  * Returns the next generated value.
  */
 export function getValue(element?: Value | Property | PropertyReference): unknown {
-    while (isProperty(element) || isPropertyReference(element)) {
-        element = isProperty(element) ? element.value : element.element.ref;
-    }
+    element = resolveValue(element);
     if (isNull(element) || !element) return null;
     if (isLiteral(element)) return element.literalValue;
     if (isList(element)) return element.values.map(getValue);
--- src/language-server/ranger-scope.ts
+++ src/language-server/ranger-scope.ts
@@ -1,6 +1,6 @@
 import { DefaultScopeProvider, EMPTY_SCOPE, ReferenceInfo, Scope } from 'langium';
 
-import { isObjekt, isPropertyReference, PropertyReference, Value } from './generated/ast';
+import { isObjekt, isProperty, isPropertyReference, Property, PropertyReference, Value } from './generated/ast';
 
 export class RangerScopeProvider extends DefaultScopeProvider {
     override getScope(context: ReferenceInfo): Scope {
@@ -11,12 +11,13 @@ export class RangerScopeProvider extends DefaultScopeProvider {
                 return super.getScope(context);
             }
             const previousValue = previousElement.value;
+            const resolved = resolveValue(previousElement); // TODO: scoping of resolved references not yet working
             return this.scopeValue(previousValue);
         }
         return super.getScope(context);
     }
 
-    private scopeValue(value: Value | PropertyReference): Scope {
+    private scopeValue(value?: Value): Scope {
         if (isObjekt(value)) {
             return this.createScopeForNodes(value.properties);
         }
@@ -24,4 +25,20 @@ export class RangerScopeProvider extends DefaultScopeProvider {
         // Simply return an empty scope
         return EMPTY_SCOPE;
     }
+}
+
+/**
+ * Resolves the Value behind a Property or PropertyReference.
+ * Supports transitive references.
+ */
+export function resolveValue(element?: Property | PropertyReference | Value): Value | undefined {
+    let i = 1;
+    while (isProperty(element) || isPropertyReference(element)) {
+        element = isProperty(element) ? element.value : element.element.ref;
+        if (i++ >= 100) {
+            console.log(`Max reference depth exceeded for element [${element?.$type}]`);
+            return undefined;
+        }
+    }
+    return element;
 }
