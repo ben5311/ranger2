0a77d99c0159fabcd92a7b288c4817b27fb9bedc
Benjamin Heimann <benjamin.heimann@outlook.de>
2023-04-10 17:07:15 +0200

    Add CSV output format

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,17 +1,21 @@
 import chalk from 'chalk';
 import { Presets, SingleBar } from 'cli-progress';
 import { Command, Option } from 'commander';
+import * as csv from 'csv-stringify';
 import fs from 'fs';
 import path from 'path';
+import * as stream from 'stream';
 
 import { RangerLanguageMetaData } from '../language-server/generated/module';
 import { createObjectGenerator } from '../language-server/ranger-generator';
 
 export type Options = {
     count: number;
-    format: 'jsonl' | 'csv';
+    format: Format;
     outputDir: string;
 };
+const formats = ['jsonl', 'csv'] as const;
+export type Format = typeof formats[number];
 
 export default function (): void {
     const program = new Command();
@@ -22,38 +26,68 @@ export default function (): void {
         .description('Generate test data from Ranger configuration file.')
         .argument('<rangerFile>', `Source file (possible file extensions: ${fileExtensions})`)
         .addOption(new Option('-c --count <count>', 'The count of rows to generate').default('10').argParser(parseIntg))
-        .addOption(new Option('-f, --format <format>', 'The output format').choices(['jsonl', 'csv']).default('jsonl'))
+        .addOption(new Option('-f, --format <format>', 'The output format').choices(formats).default('jsonl'))
         .addOption(new Option('-o, --outputDir <dir>', 'The desired output directory').default('generated'))
         .action(generateOutputFile);
+
     program.parse(process.argv);
 }
 
-// generateOutputFile('examples/User.ranger', { count: 20, format: 'jsonl', outputDir: 'generated' });
+//generateOutputFile('examples/User.ranger', { count: 20, format: 'jsonl', outputDir: 'generated' });
 
 export async function generateOutputFile(filePath: string, opts: Options): Promise<void> {
     const generator = await createObjectGenerator(filePath);
     const outputFileName = path.basename(filePath, path.extname(filePath));
     const outputFilePath = path.join(opts.outputDir, `${outputFileName}.${opts.format}`);
-    const outputFile = fs.createWriteStream(outputFilePath, { flags: 'w', encoding: 'utf-8' });
+    const outputWriter = createWriter(outputFilePath, opts.format);
 
-    if (!fs.existsSync(opts.outputDir)) fs.mkdirSync(opts.outputDir, { recursive: true });
     const progressBarFormat = ' {bar} {percentage}% | T: {duration_formatted} | ETA: {eta_formatted} | {value}/{total}';
     const progressBar = new SingleBar({ format: progressBarFormat }, Presets.shades_classic);
     progressBar.start(opts.count, 0);
 
     for (let i = 1; i <= opts.count; i++) {
         const generated = generator.next();
-        outputFile.write(JSON.stringify(generated));
-        outputFile.write('\n');
+        outputWriter.write(generated);
         progressBar.increment();
     }
 
-    outputFile.close();
+    outputWriter.end();
     progressBar.stop();
     console.log(chalk.green(`Output file generated successfully: ${outputFilePath}`));
 }
 
-export function parseIntg(text: string): number {
+export function createWriter(filePath: string, format: Format): stream.Writable {
+    const parentDir = path.dirname(filePath);
+    if (!fs.existsSync(parentDir)) {
+        fs.mkdirSync(parentDir, { recursive: true });
+    }
+    const transformer = transformers[format]();
+    const outputStream = fs.createWriteStream(filePath, { flags: 'w', encoding: 'utf-8' });
+    transformer.pipe(outputStream);
+    return transformer;
+}
+
+const transformers: { [key: string]: () => stream.Transform } = {
+    jsonl: createJsonlTransformer,
+    csv: createCsvTransformer,
+};
+
+function createJsonlTransformer(): stream.Transform {
+    return new stream.Transform({
+        objectMode: true,
+        transform(chunk, _encoding, callback) {
+            this.push(JSON.stringify(chunk));
+            this.push('\n');
+            callback();
+        },
+    });
+}
+
+function createCsvTransformer(): stream.Transform {
+    return csv.stringify({ header: true, delimiter: ',' });
+}
+
+function parseIntg(text: string): number {
     const parsedNumber = parseInt(text);
     if (isNaN(parsedNumber)) {
         console.log(chalk.red(`Not a valid integer: ${text}`));
