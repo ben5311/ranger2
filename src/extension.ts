b2ad9b24adc3fd6233e246dd4bc501cc6bcd45ce
Benjamin Heimann <benjamin.heimann@outlook.de>
2023-04-11 16:33:01 +0200

    Revert "Add inline values decorator"
    This reverts commit e034278b7b4656bf3c2c9509b3dffa331d185bc3.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
-import * as path from 'path';
 import * as vscode from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
+import * as path from 'path';
+import {
+    LanguageClient, LanguageClientOptions, ServerOptions, TransportKind
+} from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
@@ -21,16 +23,14 @@ function startLanguageClient(context: vscode.ExtensionContext): LanguageClient {
     const serverModule = context.asAbsolutePath(path.join('out', 'language-server', 'main'));
     // The debug options for the server
     // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging.
-    // By using --inspect-brk instead of --inspect, the language server will wait until a debugger is attached.
-    const inspectMode = process.env.DEBUG_BREAK ? '--inspect-brk' : '--inspect';
-    const debugPort = process.env.DEBUG_SOCKET || '6009';
-    const debugOptions = { execArgv: ['--nolazy', `${inspectMode}=${debugPort}`] };
+    // By setting `process.env.DEBUG_BREAK` to a truthy value, the language server will wait until a debugger is attached.
+    const debugOptions = { execArgv: ['--nolazy', `--inspect${process.env.DEBUG_BREAK ? '-brk' : ''}=${process.env.DEBUG_SOCKET || '6009'}`] };
 
     // If the extension is launched in debug mode then the debug server options are used
     // Otherwise the run options are used
     const serverOptions: ServerOptions = {
         run: { module: serverModule, transport: TransportKind.ipc },
-        debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions },
+        debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
     };
 
     const fileSystemWatcher = vscode.workspace.createFileSystemWatcher('**/*.ranger');
@@ -41,59 +41,19 @@ function startLanguageClient(context: vscode.ExtensionContext): LanguageClient {
         documentSelector: [{ scheme: 'file', language: 'ranger' }],
         synchronize: {
             // Notify the server about file changes to files contained in the workspace
-            fileEvents: fileSystemWatcher,
-        },
-        middleware: {
-            // Decorate inline values for currently active Ranger document.
-            async provideDocumentSymbols(document, token, next) {
-                const documentSymbols = await next(document, token);
-                const activeDocument = vscode.window.activeTextEditor?.document;
-                if (document.uri === activeDocument?.uri) {
-                    const decorations: vscode.DecorationOptions[] = [];
-                    addDecorations(documentSymbols, decorations);
-                    vscode.window.activeTextEditor?.setDecorations(decorationTemplate, decorations);
-                    ILanguageFeaturesService;
-                }
-                return documentSymbols;
-            },
-        },
+            fileEvents: fileSystemWatcher
+        }
     };
 
     // Create the language client and start the client.
-    const client = new LanguageClient('ranger', 'Ranger', serverOptions, clientOptions);
+    const client = new LanguageClient(
+        'ranger',
+        'Ranger',
+        serverOptions,
+        clientOptions
+    );
 
     // Start the client. This will also launch the server
     client.start();
     return client;
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Inline values decorator service
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-type Symbols = vscode.SymbolInformation[] | vscode.DocumentSymbol[] | null | undefined;
-function addDecorations(symbols: Symbols, decorations: vscode.DecorationOptions[]) {
-    for (const symbol of symbols || []) {
-        if ('selectionRange' in symbol) {
-            const range = symbol.selectionRange;
-            decorations.push({
-                range: new vscode.Range(range.start.line, range.start.character, range.end.line, Number.MAX_VALUE),
-                renderOptions: {
-                    after: { contentText: symbol.detail },
-                },
-            });
-            if (symbol.children) {
-                addDecorations(symbol.children, decorations);
-            }
-        }
-    }
-}
-
-const decorationTemplate = vscode.window.createTextEditorDecorationType({
-    after: {
-        color: new vscode.ThemeColor('ranger.inlineText'),
-        fontStyle: 'normal',
-        fontWeight: 'normal',
-        margin: '0 0 0 4ch',
-    },
-});
