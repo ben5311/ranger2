d45859c38805370174e4e7dcb3d053e1e4641691
Benjamin Heimann <benjamin.heimann@outlook.de>
2023-04-17 19:52:23 +0200

    Improve syntax highlighting

--- src/language-server/ranger-tokens.ts
+++ src/language-server/ranger-tokens.ts
@@ -5,12 +5,21 @@ import * as ast from './generated/ast';
 
 export class RangerTokenProvider extends AbstractSemanticTokenProvider {
     protected override highlightElement(node: AstNode, highlight: SemanticTokenAcceptor): void | 'prune' | undefined {
-        if (isPureProperty(node)) {
-            highlight({ node: node, property: 'name', type: 'property' });
+        if (ast.isNull(node)) {
+            highlight({ node, keyword: 'null', type: 'keyword' });
         } else if (ast.isPrimitive(node) && typeof node.value === 'boolean') {
-            highlight({ node: node, property: 'value', type: 'keyword' });
+            highlight({ node, property: 'value', type: 'keyword' });
+        } else if (ast.isPrimitive(node) && typeof node.value === 'string') {
+            highlight({ node, property: 'value', type: 'string' });
         } else if (ast.isNum(node)) {
-            highlight({ node: node, property: 'value', type: 'number' });
+            highlight({ node, property: 'value', type: 'number' });
+        } else if (isPureProperty(node)) {
+            highlight({ node, property: 'name', type: 'property' });
+        } else if (ast.isEntity(node)) {
+            highlight({ node, keyword: 'Entity', type: 'keyword' });
+        } else if (ast.isFunc(node)) {
+            const match = node.$cstNode?.text?.match(/([\w_]+)\(/);
+            if (match) highlight({ node, keyword: match[1], type: 'keyword' });
         }
     }
 }
