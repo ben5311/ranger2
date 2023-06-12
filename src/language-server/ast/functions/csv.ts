23a2950e5a7a5449cfd5a81d7da015f33ab4d00d
Benjamin Heimann <benjamin.heimann@outlook.de>
2023-06-12 21:52:04 +0200

    Cache CSV Rows to avoid editor lags

--- src/language-server/ast/functions/csv.ts
+++ src/language-server/ast/functions/csv.ts
@@ -1,6 +1,7 @@
 import * as csv from 'csv/sync';
 import fs from 'fs';
 import { ValidationAcceptor } from 'langium';
+import { LRUCache } from 'lru-cache';
 
 import { CsvFunc } from '../../generated/ast';
 import { resolvePath } from '../../ranger-documents';
@@ -10,7 +11,28 @@ import { ValueGenerator } from '../ValueGenerator';
 import { FuncCompanion, FuncHover } from './func';
 
 export class CsvFuncCompanion extends FuncCompanion<CsvFunc> {
+    cache: LRUCache<string, any[]> = new LRUCache({ max: 100, ttl: 60 * 1000 });
+
     override valueGenerator(csvFunc: CsvFunc): ValueGenerator {
+        const rows = this.getRows(csvFunc);
+
+        return new ValueGenerator(() => {
+            const randomIndex = this.generator.random.integer(0, rows.length - 1);
+            return rows[randomIndex];
+        });
+    }
+
+    protected getRows(csvFunc: CsvFunc): any[] {
+        const filePath = resolvePath(csvFunc.filePath.value, csvFunc);
+
+        if (!this.cache.has(filePath)) {
+            this.cache.set(filePath, this.parseCsvFile(csvFunc));
+        }
+
+        return this.cache.get(filePath)!;
+    }
+
+    protected parseCsvFile(csvFunc: CsvFunc): any[] {
         const filePath = resolvePath(csvFunc.filePath.value, csvFunc);
         const data = fs.readFileSync(filePath, 'utf-8');
 
@@ -21,12 +43,7 @@ export class CsvFuncCompanion extends FuncCompanion<CsvFunc> {
             csvFunc.delimiter = delimiter;
         }
 
-        const rows: any[] = csv.parse(data, { delimiter: csvFunc.delimiter, columns: !csvFunc.noHeader });
-
-        return new ValueGenerator(() => {
-            const randomIndex = this.generator.random.integer(0, rows.length - 1);
-            return rows[randomIndex];
-        });
+        return csv.parse(data, { delimiter: csvFunc.delimiter, columns: !csvFunc.noHeader });
     }
 
     override funcHover(csvFunc: CsvFunc): FuncHover {
